be89dbe6afd7cc9fd64a9be93ce258ba164a32b4
CCTV01_PhongVu <phungtienviet14@gmail.com>
2024-04-02 18:50:59 +0700

    02/04/v.1

--- Sonic/app/src/main/java/com/example/sonic/adapter/PlaylistAdapter.java
+++ Sonic/app/src/main/java/com/example/sonic/adapter/PlaylistAdapter.java
@@ -1,51 +1,89 @@
 package com.example.sonic.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.helper.widget.Layer;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sonic.R;
-import com.example.sonic.adapter.model.Home;
+import com.example.sonic.network.model.ArtistDTO;
 import com.example.sonic.network.model.Lib;
 import com.example.sonic.network.model.PlaylistDTO;
+import com.example.sonic.network.remote.APIServiceToken;
+import com.example.sonic.network.remote.RetrofitClient;
+import com.example.sonic.network.remote.RetrofitClientToken;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class PlaylistAdapter extends  RecyclerView.Adapter<Home> {
+public class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.PlaylistViewHolder> {
 
-    public static List<Home>data;
+    public static List<Lib> data;
+//    private Context mContext;
+//
+//    public PlaylistAdapter(Context mContext) {
+//        this.mContext = mContext;
+//    }
+    public void setData(List<Lib> data){
+        this.data=data;
+        notifyDataSetChanged();
+    }
 
     @NonNull
     @Override
-    public Home onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public PlaylistViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_playlist_home, parent, false);
+        return new PlaylistViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Home holder, int position) {
-
+    public void onBindViewHolder(@NonNull PlaylistViewHolder holder, int position) {
+        Lib lib=data.get(position);
+        if(lib==null){
+            return;
+        }
+       if( lib.getArtistDTO()!=null){
+           ArtistDTO artistDTO= lib.getArtistDTO();
+           holder.textViewCategoryHomeItem.setText("Nghệ sĩ");
+           holder.textViewNameHomeItem.setText(artistDTO.getName());
+           Picasso.get().load(RetrofitClient.url+artistDTO.getImage()).into(holder.imageViewHomeItem);
+       }else{
+           PlaylistDTO playlistDTO=lib.getPlaylistDTO();
+           holder.textViewCategoryHomeItem.setText("Danh sách phát");
+           holder.textViewNameHomeItem.setText(playlistDTO.getName());
+           Picasso.get().load(RetrofitClient.url+playlistDTO.getImage()).into(holder.imageViewHomeItem);
+       }
     }
 
     @Override
     public int getItemCount() {
+        if (data != null) {
+            return data.size();
+        }
         return 0;
     }
 
-    class PlaylistViewHolder extends RecyclerView.ViewHolder {
-        ImageView mImageViewHomeItem;
-        TextView mTextViewNameHomeItem;
-        TextView mTextViewCategoryHomeItem;
+    public void setData() {
+        APIServiceToken apiServiceToken = RetrofitClientToken.getClientToken(null).create(APIServiceToken.class);
+    }
+
+    public class PlaylistViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageViewHomeItem;
+        private TextView textViewNameHomeItem;
+        private TextView textViewCategoryHomeItem;
 
 
         public PlaylistViewHolder(@NonNull View itemView) {
             super(itemView);
-            mImageViewHomeItem=itemView.findViewById(R.id.img_view_playlist_home_item);
-            mTextViewNameHomeItem=itemView.findViewById(R.id.text_name_playlist_home_item);
-            mTextViewCategoryHomeItem=itemView.findViewById(R.id.text_category_playlist_home_item);
+            imageViewHomeItem = itemView.findViewById(R.id.img_view_playlist_home_item);
+            textViewNameHomeItem = itemView.findViewById(R.id.text_name_playlist_home_item);
+            textViewCategoryHomeItem = itemView.findViewById(R.id.text_category_playlist_home_item);
         }
     }
 }
