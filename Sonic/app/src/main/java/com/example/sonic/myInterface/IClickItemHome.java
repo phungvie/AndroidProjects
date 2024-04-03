a9cc7f2eccbd4d0d92b32c5f6b85af2d92896019
CCTV01_PhongVu <phungtienviet14@gmail.com>
2024-04-03 18:24:10 +0700

    phiên bản này ko có gì ngoài thêm myInterface

---
+++ Sonic/app/src/main/java/com/example/sonic/myInterface/IClickItemHome.java
@@ -0,0 +1,7 @@
+package com.example.sonic.myInterface;
+
+import com.example.sonic.model.ArtistAndPlaylist;
+
+public interface IClickItemViet {
+    void onClickIteam(ArtistAndPlaylist artistAndPlaylist);
+}
